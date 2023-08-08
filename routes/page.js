51504844b71e0c77b0dded1c6cf0d48791a4cf94
EdrissAria <edrissalokozay77@gmail.com>
2023-08-08 14:28:34 -0400

    get data from database and show it

--- routes/page.js
+++ routes/page.js
@@ -1,5 +1,69 @@
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  FormField,
+  Icon,
+  Input,
+  Row,
+  View,
+} from "@ulibs/ui";
+
 export async function load({ ctx }) {
-    return {
-        name: "ssss"
-    };
+  const todos = await ctx.table("todos").query();
+
+  if (!todos) {
+    return {};
   }
+
+  return {todos: todos.data};
+}
+
+export async function update({ ctx, body }) {
+  await ctx.table("todos").update(body.id, {
+    todo: body.todo,
+    completed: body.completed,
+  });
+
+  return {
+    body: {
+      success: true,
+    },
+  };
+}
+
+export async function add({ ctx, body }) {
+  const page = {
+    todo: body.todo,
+    completed: body.completed,
+  };
+
+  await ctx.table("todos").insert(page);
+
+  return {
+    body: {
+      success: true,
+    },
+  };
+}
+
+export default ({todos}) => {
+  return [
+    View([
+        View(todos.map(todo=>View(todo.todo))),
+        Form({$data: {todo: '', completed: false}}, [
+            Input({ name: "username", label: 'Username' }),
+          
+            Col({ col: 12, mb: "sm" }, [
+              View({ border: true, borderColor: 'base-400', p: "sm"}, [
+                View(["Username: ", View({ $text: "username" })]),
+              ]),
+            ]),
+            Col({ justify: 'end'}, [
+              Button({ type: "submit", color: 'primary' }, "Submit"),
+            ])
+          ])
+    ])
+  ];
+};
